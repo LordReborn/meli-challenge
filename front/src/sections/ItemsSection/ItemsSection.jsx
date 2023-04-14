aad98d7b41562c404459e9023fe4facc558cfbfa
Jonas Aguilar <jonasaguilar2000@gmail.com>
2023-04-14 01:10:30 -0300

    [MC-4] Fetching de datos en listado de Items

--- front/src/sections/ItemsSection/ItemsSection.jsx
+++ front/src/sections/ItemsSection/ItemsSection.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { settings } from '../../settings'
 
 const ItemsSection = () => {
-  return (
-    <div>ItemsSection</div>
-  )
+  const [data, setData] = useState(null)
+  const [searchParams] = useSearchParams()
+
+  useEffect(() => {
+    const abortController = new AbortController()
+
+    const getData = async () => {
+      const query = searchParams.get('q')
+      const url = `${settings.API_URL}/api/items?q=${query}`
+      try {
+        const res = await fetch(url, { signal: abortController.signal })
+        const json = await JSON.parse(res)
+        setData(json)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    getData()
+
+    return () => {
+      abortController.abort()
+    }
+  }, [searchParams])
+
+  return <div>ItemsSection</div>
 }
 
 export default ItemsSection
---
+++ front/src/settings.js
@@ -0,0 +1,3 @@
+export const settings = {
+  API_URL: import.meta.env.VITE_API_BASE_URL
+}
