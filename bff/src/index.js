2426af45270e795153845ff2ac545d44fd74f1d5
Jonas Aguilar <jonasaguilar2000@gmail.com>
2023-04-13 21:23:45 -0300

    [MC-1][BFF] Se agregan EP para solicitar info.

--- bff/src/index.js
+++ bff/src/index.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const fetch = require('node-fetch')
+const adapter = require('./utils/adapter')
 const app = express()
 const port = 3000
 
-app.get('/ping', (req, res) => {
+app.get('/ping', (_req, res) => {
   res.send('pong')
+})
+
+app.get('/api/items', (req, res) => {
+  const { q } = req.query
+  let url = 'https://api.mercadolibre.com/sites/MLA/search'
+  if (q) url += `?q=${q}`
+  fetch(url).then((response) => response.json()).then((data) => res.send(adapter.list(data))).catch((e) => res.send(e))
+})
+
+app.get('/api/items/:id', (req, res) => {
+  const { id } = req.params
+  const url1 = `https://api.mercadolibre.com/items/${id}`
+  const url2 = `https://api.mercadolibre.com/items/${id}/description`
+  const promise1 = fetch(url1).then((response) => response.json())
+  const promise2 = fetch(url2).then((response) => response.json())
+
+  Promise.all([promise1, promise2]).then(data => res.send(adapter.show(data)))
 })
 
 app.listen(port, () => {
---
+++ bff/src/utils/adapter.js
@@ -0,0 +1,48 @@
+const adapter = (() => {
+  const list = (data) => {
+    const author = { name: 'Jonás', lastname: 'Aguilar' }
+
+    const categories = data.filters[0].values[0].path_from_root?.map((categorie) => categorie.name) || []
+    const items = data.results.map((item) => {
+      const [amount, decimals] = `${item.price}`.split('.')
+      return {
+        id: item.id,
+        title: item.title,
+        prices: {
+          currency: item.currency_id,
+          amount: parseInt(amount),
+          decimals: parseInt(decimals || 0)
+        },
+        picture: item.thumbnail,
+        condition: item.condition,
+        free_shipping: item.shipping.free_shipping
+      }
+    })
+    return { author, categories, items }
+  }
+
+  const show = ([itemData, description]) => {
+    const [amount, decimals] = `${itemData.price}`.split('.')
+
+    const author = { name: 'Jonás', lastname: 'Aguilar' }
+    const item = {
+      id: itemData.id,
+      title: itemData.id,
+      price: {
+        currency: itemData.currency_id,
+        amount: parseInt(amount),
+        decimals: parseInt(decimals)
+      },
+      picture: itemData.pictures[0].url,
+      condition: itemData.condition,
+      free_shipping: itemData.shipping.free_shipping,
+      sold_quantity: itemData.sold_quantity,
+      description: description.plain_text
+    }
+    return { author, item }
+  }
+
+  return { list, show }
+})()
+
+module.exports = adapter
