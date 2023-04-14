1bcea3bc0a56afa9ea974892168a352cf1b4264b
Jonas Aguilar <jonasaguilar2000@gmail.com>
2023-04-14 16:57:53 -0300

    [MC-7] Fetching de datos en consulta de item

--- front/src/sections/ItemSection/ItemSection.jsx
+++ front/src/sections/ItemSection/ItemSection.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { settings } from '../../settings'
 
 const ItemSection = () => {
-  return (
-    <div>ItemSection</div>
-  )
+  const [data, setData] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const abortController = new AbortController()
+
+    const getData = async () => {
+      const url = `${settings.API_URL}/api/items/${id}`
+      try {
+        const res = await fetch(url, { signal: abortController.signal })
+        const json = await res.json()
+        setData(json)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    getData()
+
+    return () => {
+      abortController.abort()
+    }
+  }, [id])
+
+  return <div></div>
 }
 
 export default ItemSection
