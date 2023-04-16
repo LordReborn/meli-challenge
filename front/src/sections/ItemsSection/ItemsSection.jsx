9ad4e167794193ace16c67ee88b3c5d17990bf18
Jonas Aguilar <jonasaguilar2000@gmail.com>
2023-04-16 18:06:14 -0300

    [Fix] Mensaje de carga y sin resultados

--- front/src/sections/ItemsSection/ItemsSection.jsx
+++ front/src/sections/ItemsSection/ItemsSection.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { Breadcrumbs, ListItems } from '../../components'
 import { settings } from '../../settings'
+import { isObjectEmpty } from '../../utils/common'
 
 const ItemsSection = () => {
   const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
   const [searchParams] = useSearchParams()
 
   useEffect(() => {
@@ -13,12 +15,15 @@ const ItemsSection = () => {
     const getData = async () => {
       const query = searchParams.get('q')
       const url = `${settings.API_URL}/api/items?q=${query}`
+      setLoading(true)
       try {
         const res = await fetch(url, { signal: abortController.signal })
         const json = await res.json()
         setData(json)
       } catch (e) {
         console.error(e)
+      } finally {
+        setLoading(false)
       }
     }
 
@@ -29,7 +34,9 @@ const ItemsSection = () => {
     }
   }, [searchParams])
 
-  if (!data) return null
+  if (loading) return <h2>Cargando ...</h2>
+
+  if (isObjectEmpty(data)) return <h2>Sin resultados</h2>
 
   return (
     <>
--- front/src/utils/common.js
+++ front/src/utils/common.js
@@ -3,3 +3,7 @@ export const getDecimals = (decimals) => {
   if (decimals.length === 2) return `${decimals}0`
   return `${decimals}`
 }
+
+export const isObjectEmpty = (objectName) => {
+  return objectName && Object.keys(objectName).length === 0
+}
